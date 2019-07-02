85d55b8e0857c319c856b2f4827cc06345dc6e58
Javier Pascual <javier.pascual@media4care.de>
2019-07-02 12:28:45 +0200

    Generate types

---
+++ src/graphql/types.ts
@@ -0,0 +1,141 @@
+/* eslint-disable */
+import gql from "graphql-tag";
+import * as ReactApolloHooks from "react-apollo-hooks";
+export type Maybe<T> = T | null;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  Date: any;
+};
+
+export type Chat = {
+  __typename?: "Chat";
+  id: Scalars["ID"];
+  name: Scalars["String"];
+  picture?: Maybe<Scalars["String"]>;
+  lastMessage?: Maybe<Message>;
+  messages: Array<Message>;
+};
+
+export type Message = {
+  __typename?: "Message";
+  id: Scalars["ID"];
+  content: Scalars["String"];
+  createdAt: Scalars["Date"];
+  chat?: Maybe<Chat>;
+};
+
+export type Mutation = {
+  __typename?: "Mutation";
+  addMessage?: Maybe<Message>;
+};
+
+export type MutationAddMessageArgs = {
+  chatId: Scalars["ID"];
+  content: Scalars["String"];
+};
+
+export type Query = {
+  __typename?: "Query";
+  chats: Array<Chat>;
+  chat?: Maybe<Chat>;
+};
+
+export type QueryChatArgs = {
+  chatId: Scalars["ID"];
+};
+
+export type Subscription = {
+  __typename?: "Subscription";
+  chatId: Scalars["ID"];
+  messageAdded: Message;
+};
+export type ChatFragment = { __typename?: "Chat" } & Pick<
+  Chat,
+  "id" | "name" | "picture"
+>;
+
+export type MessageFragment = { __typename?: "Message" } & Pick<
+  Message,
+  "id" | "createdAt" | "content"
+>;
+
+export type FullChatFragment = { __typename?: "Chat" } & {
+  messages: Array<{ __typename?: "Message" } & MessageFragment>;
+} & ChatFragment;
+
+export type GetChatsQueryVariables = {};
+
+export type GetChatsQuery = { __typename?: "Query" } & {
+  chats: Array<{ __typename?: "Chat" } & ChatFragment>;
+};
+
+export type GetChatQueryVariables = {
+  chatId: Scalars["ID"];
+};
+
+export type GetChatQuery = { __typename?: "Query" } & {
+  chat: Maybe<{ __typename?: "Chat" } & FullChatFragment>;
+};
+export const ChatFragmentDoc = gql`
+  fragment Chat on Chat {
+    id
+    name
+    picture
+  }
+`;
+export const MessageFragmentDoc = gql`
+  fragment Message on Message {
+    id
+    createdAt
+    content
+  }
+`;
+export const FullChatFragmentDoc = gql`
+  fragment FullChat on Chat {
+    ...Chat
+    messages {
+      ...Message
+    }
+  }
+  ${ChatFragmentDoc}
+  ${MessageFragmentDoc}
+`;
+export const GetChatsDocument = gql`
+  query GetChats {
+    chats {
+      ...Chat
+    }
+  }
+  ${ChatFragmentDoc}
+`;
+
+export function useGetChatsQuery(
+  baseOptions?: ReactApolloHooks.QueryHookOptions<GetChatsQueryVariables>
+) {
+  return ReactApolloHooks.useQuery<GetChatsQuery, GetChatsQueryVariables>(
+    GetChatsDocument,
+    baseOptions
+  );
+}
+export const GetChatDocument = gql`
+  query GetChat($chatId: ID!) {
+    chat(chatId: $chatId) {
+      ...FullChat
+    }
+  }
+  ${FullChatFragmentDoc}
+`;
+
+export function useGetChatQuery(
+  baseOptions?: ReactApolloHooks.QueryHookOptions<GetChatQueryVariables>
+) {
+  return ReactApolloHooks.useQuery<GetChatQuery, GetChatQueryVariables>(
+    GetChatDocument,
+    baseOptions
+  );
+}
